6189d2b269a577588975ada4d0a1512da6f6a800
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-11-01 15:03:41 +0000

    build the create_df() function and accompanying passing test

--- src/json_data.py
+++ src/json_data.py
@@ -1,8 +1,14 @@
+import pandas as pd
+
+
 def create_df(filename):
-    pass
+    df = pd.read_json(filename)
+    return pd.json_normalize(df["doughnut_data"])
+
 
 def increase_price(df):
     pass
+
 
 def get_best_value(df):
     pass
--- tests/test_json_data.py
+++ tests/test_json_data.py
@@ -1,2 +1,15 @@
 from src.json_data import create_df, increase_price, get_best_value
+import pandas as pd
 
+
+class TestCreateDf:
+    def test_creates_df_from_json(self):
+        # Act
+        test_input = "./data/doughnuts.json"
+        expected_columns = ["doughnut_type", "price", "calories", "contains_nuts"]
+        # Assert
+        result = create_df(test_input)
+        # Act
+        assert isinstance(result, pd.DataFrame)
+        assert list(result.columns) == expected_columns
+        assert len(result) == 10
